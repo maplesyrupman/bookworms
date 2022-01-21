402546e68ec17d3d7590110638eee0fb8a80e1bf
maplesyrupman <maplesyrupman@protonmail.com>
2022-01-20 21:49:24 -0500

    logup form

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,44 @@
 import './index.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+import Cookies from 'js-cookie'
+
 import Nav from './pages/Nav';
 import Home from './pages/Home';
 import Logup from './pages/Logup';
 
+const httpLink = createHttpLink({
+  uri: '/graphql'
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = Cookies.get('token')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    }
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache({})
+})
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Nav />}>
-          <Route index element={<Home /> } />
-          <Route path='logup' element={<Logup />} />
-        </Route>
-      </Routes>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Nav />}>
+            <Route index element={<Home />} />
+            <Route path='logup' element={<Logup />} />
+          </Route>
+        </Routes>
+      </Router>
+    </ApolloProvider>
   );
 }
 
