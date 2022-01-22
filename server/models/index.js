0b1be506c2bc8d61ce7e52272d19e80ea242d7e8
Varsha Jain <csvarshajain@gmail.com>
2022-01-22 18:45:01 -0500

    added bookclub model and queries related to it

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,4 @@
 const User = require('./User');
+const BookClub = require('./Bookclub')
 
-module.exports = { User };
+module.exports = { User, BookClub };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -11,10 +11,34 @@ const typeDefs = gql`
         username: String!
 		email: String!
 		password: String!
+        bookClubs: [BookClub]
     }
 
     type Query {
         users: [User]
+    }
+
+    type BookClub {
+        _id: ID
+        bookClubName: String!
+        createdAt: String
+        username: String
+        discussionCount: Int
+        discussions : [Discussion]
+    }
+    type Discussion {
+        _id: ID
+        discussionBody: String
+        createdAt: String
+        username: String
+      }
+
+    type Query {
+        users:[User]
+        user(username: String!): User
+        bookClubs : [BookClub]
+        bookClubs(username: String) : [BookClub]
+        bookClub(_id: ID!): BookClub
     }
 
     type Mutation {
