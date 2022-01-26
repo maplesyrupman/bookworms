d177acb4c6b3a1deaa169024aebb6a649a96080c
dan13l80 <88808310+dan13l80@users.noreply.github.com>
2022-01-25 20:05:16 -0500

    Event added

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const BookClub = require('./Bookclub');
 const Book = require('./Book')
+const Event = require('./Event')
 
-module.exports = { User, BookClub, Book };
+module.exports = { User, BookClub, Book, Event };
